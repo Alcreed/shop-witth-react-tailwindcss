12547e682082bc1c45f6f7fcb1e9d3d5feed6d38
Alfonso Gonzalez <alfonso_gonzalezg@outlook.com>
2023-12-11 16:39:14 -0500

    Create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+
 import { Home } from './pages/Home/Home.jsx'
-import { MyAccount } from './pages/MyAccount/MyAccount.jsx'
 import { MyOrder } from './pages/MyOrder/MyOrder.jsx'
 import { MyOrders } from './pages/MyOrders/MyOrders.jsx'
+import { MyAccount } from './pages/MyAccount/MyAccount.jsx'
 import { SignIn } from './pages/SignIn/SignIn.jsx'
 import { NotFound } from './pages/NotFound/NotFound.jsx'
 
 import './App.css'
 
+const Routes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '*', element: <NotFound /> },
+  ])
+
+  return routes
+}
+
 function App() {
   return (
     <>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <SignIn />
-      <NotFound />
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
     </>
   )
 }
