67af6f20a8df1eab41a469a7b6f6c1d09b3dba49
Alfonso Gonzalez <alfonso_gonzalezg@outlook.com>
2023-12-13 12:12:18 -0500

    Api request and update card

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,41 @@
+import { useState, useEffect } from "react";
+
 import { Card } from "../../components/Card/Card";
 
 function Home() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        console.log("🚀 ~ file: Home.jsx:13 ~ fetchData ~ data:", data)
+        setItems(data);
+      } catch (error) {
+        console.log(`Hubo un error: ${error}`);
+      }
+    }
+    fetchData();
+  }, [])
+
   return (
     <div>
       Home
-      <Card />
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {items.length > 0 ?
+          items.map(item => {
+            return (
+              <Card
+                key={item.id}
+                data={item}
+              />
+            )
+          })
+        :
+          null
+        }
+      </div>
     </div>
   );
 }
